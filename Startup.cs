3e6f090c598ff7489444e7f78cbe5fef1045bc22
Rafael Castillo <rafaelcastillo@outlook.com>
2019-09-02 11:41:39 -0500

    Implementing API using Sqlite (working)

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using AspNetCore;
 
 namespace food_records
 {
@@ -25,11 +28,33 @@ namespace food_records
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddCors(options =>
+            {
+            options.AddPolicy("VueCorsPolicy", builder =>
+                {
+                builder
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+                    .WithOrigins("http://localhost:8080");
+                });
+            });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+            	{
+                	options.Authority = Configuration["Okta:Authority"];
+                	options.Audience = "api://default";
+            	});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext dbContext)
         {
             if (env.IsDevelopment())
             {
@@ -39,6 +64,11 @@ namespace food_records
             {
                 app.UseHsts();
             }
+
+            app.UseCors("VueCorsPolicy");
+            app.UseAuthentication();
+            
+            dbContext.Database.EnsureCreated();
 
             app.UseHttpsRedirection();
             app.UseMvc();
